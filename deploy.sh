e3df85dec8c2ebfab7345fa9e53eb210491c22dc
Irfan Adilovic <irfanadilovic@gmail.com>
2017-03-15 17:59:29 +0100

    async uninstall/install of apk

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+adb uninstall ba.vaktija.android.irfan &
 apktool b ba.vaktija.android
 echo android | jarsigner -tsa http://timestamp.digicert.com ba.vaktija.android/dist/ba.vaktija.android.apk android
-#cp -v ba.vaktija.android/dist/ba.vaktija.android.apk /cygdrive/c/Users/root/Google\ Drive/ba.vaktija.android.irfan.apk
-adb uninstall ba.vaktija.android.irfan
-adb install ba.vaktija.android/dist/ba.vaktija.android.apk
+wait
+adb install ba.vaktija.android/dist/ba.vaktija.android.apk &
+cp -v ba.vaktija.android/dist/ba.vaktija.android.apk /cygdrive/c/Users/root/Google\ Drive/ba.vaktija.android.irfan.apk
+wait
