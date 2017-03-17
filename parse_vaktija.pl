d20c13507c81952670e16b42d2f8380302a82298
Irfan Adilovic <irfanadilovic@gmail.com>
2017-03-18 00:58:54 +0100

    Prototype line deduction for each month with some fuzziness around substring endings
    Also, new types of overrides

--- parse_vaktija.pl
+++ parse_vaktija.pl
@@ -8,6 +8,12 @@ use Data::Dumper;
 use File::Slurp;
 use JSON::XS;
 
+my @prayer_names = qw(fajr sunrise dhuhr asr maghrib isha);
+my $partial_times_regex = qr{( +\d\d){5}$};
+
+my $debug = ($ARGV[0] // '') eq '-d' || ($ARGV[1] // '') eq '-d';
+shift @ARGV if ($ARGV[0] // '') eq '-d';
+
 my $year = $ARGV[0] // `echo -n \$(date +%Y)`;
 my $conf = decode_json(read_file('conf.json'))->{$year};
 
@@ -16,39 +22,70 @@ open (my $csv, '>', $ARGV[1] // "vaktija_$year.csv");
 
 my $month = 0;
 my %prev_times;
-my $overrides = $conf->{overrides};
 my $nrecords = 0;
+my $prototype;
 while (my $line = <$fh>) {
-    next unless $line =~ /^\s{1,5}\d{1,2}.*\d{2}$/;
-    $nrecords++;
-#0    5    -100 -95  -90  -85  -80  -75  -70  -65  -60  -55  -50  -45  -40  -35  -30  -25  -20  -15  -10  -5  -1
-#   1.       5          18   6                   55   12       45   15          53   18        33   20        04
-#   1.       4          53   6                   30   12       08   15          08   17        44   19        15
-    my $day = substr($line, 0, 6);
-    ($day) = ($day =~ /(\d+)/) or die "No day could be matched";
-    $month++ if $day == 1;
+    chomp $line;
+    next unless $line =~ /^[\s.]{0,8}(\d...)/; # some lines may be split in two
+    print("matched line [$line]\n") if $debug;
+    my $day = $1;
+    $day =~ s/[^\d]//g; # day digits can have a space/dot interleaved
+    print("matched day [$day]\n") if $debug;
+
+    unless ($line =~ /$partial_times_regex/) {
+        # line may be split in two, read and parse next line
+        $line = <$fh>;
+        chomp $line;
+        $line =~ /$partial_times_regex/ or die "could not match remainder of line, even after trying the next line [$line]";
+    }
+
+    if ($day == 1) {
+        $month++;
+        $prototype = deduce_prototype($line, $month);
+    }
+    die "No prototype for first-ever line" unless $prototype;
+
+    $line = substr($line, -$prototype->{length}); # shorten line according to prototype => avoid utf8 chars
+    print "processing shortened line [$line]\n" if $debug;
+
     printf("%02d.%02d.", $day, $month);
-    printf $csv "%d,%02d-%02d", $nrecords, $month, $day;
+    printf $csv "%d,%02d-%02d", ++$nrecords, $month, $day;
 
     my %times = (day => $day, month => $month);
-    for (['fajr', -99, -88], ['sunrise', -83, -63], ['dhuhr', -58, -49], ['asr', -44, -32], ['maghrib', -27, -17], ['isha', -12, -2]) {
-        my ($id, $hcenter, $mcenter) = @$_;
-        my ($h, $m);
-        for ([\$h, $hcenter], [\$m, $mcenter]) {
-            my ($var, $center) = @$_;
-            my $substr = substr($line, $center-2, 5);
-            die "Invalid match [$substr] on date [$day.$month.]" if $substr =~ /[^\s]\s+[^\s]/;
-            ($$var) = $substr =~ /(\d+)/;
+    for my $prayer (@prayer_names) {
+        for my $time (qw(hours minutes)) {
+            my ($idx, $len) = @{$prototype->{$prayer}{$time}};
+            # reduce length by 1 if we end up in the middle of a number
+            my $substr = substr($line, $idx, $len);
+            my $nextidx = $idx+$len;
+            my $nextchar = $nextidx < length($line) ? substr($line, $nextidx, 1) : ' ';
+            if (substr($substr, -1) =~ /\d/ && $nextchar =~ /\d/) {
+                print "\nreducing substr [$substr] length by 1 due to in-the-middle ending" if $debug;
+                $substr =~ s/.$//;
+                die "unexpected non-whitespace at end of reduced substr [$substr]" unless $substr =~ / $/;
+            }
+            print("\n$prayer $time substring is [$substr]\n") if $debug;
+            #die "Invalid $prayer $time match [$substr] on date [$day.$month.]" if $substr =~ /[^\s]\s+[^\s]/;
+            ($times{$prayer}{$time}) = $substr =~ /(\d+)/;
         }
-        $times{$id}{hours} = $h // $prev_times{$id}{hours};
-        $times{$id}{minutes} = $m;
-        printf(" %02d:%02d", $times{$id}{hours}, $times{$id}{minutes});
-        if (my $override = $overrides->{sprintf("%02d-%02d-%s", $day, $month, $id)}) {
-            $times{$id} = $override;
-            printf("[override: %02d:%02d]", $times{$id}{hours}, $times{$id}{minutes});
+        $times{$prayer}{hours} //= $prev_times{$prayer}{hours};
+        for (qw(hours minutes)) {
+            my $ref = \$times{$prayer}{$_};
+            $$ref = defined $$ref ? sprintf "%02d", $$ref : '?';
         }
-        sanity_check($year, $id, \%prev_times, \%times) if $prev_times{$id};
-        printf $csv ",%02d:%02d", $times{$id}{hours}, $times{$id}{minutes};
+        print(" $times{$prayer}{hours}:$times{$prayer}{minutes}");
+
+        if (my $override = $conf->{single}{sprintf("%02d-%02d-%s", $day, $month, $prayer)}) {
+            $times{$prayer} = $override;
+            printf("[override: %02d:%02d]", $times{$prayer}{hours}, $times{$prayer}{minutes});
+        } elsif ($override = $conf->{ranges}{sprintf("%02d-%s", $month, $prayer)}) {
+            if ($override->{start} <= $day && $day <= $override->{end}) {
+                $times{$prayer} = $override; # ignore extra start/end
+                printf("[override: %02d:%02d]", $times{$prayer}{hours}, $times{$prayer}{minutes});
+            }
+        }
+        sanity_check($year, $prayer, \%prev_times, \%times);
+        printf $csv ",%02d:%02d", $times{$prayer}{hours}, $times{$prayer}{minutes};
     }
     print "\n";
     print $csv "\n";
@@ -58,44 +95,71 @@ while (my $line = <$fh>) {
 close($csv);
 close($fh);
 
+sub deduce_prototype {
+    my ($line, $month) = @_;
+    # check if there is a prototype override in config.
+    # make sure to begin with two spaces to match the regex below
+    # for consistency with other overrides, use 01 for january, etc.
+    my $override = $conf->{prototypes}{sprintf("%02d", $month)};
+    $line = $override if $override;
+
+    $line =~ /  \d +\d\d(?: +\d\d? +\d\d){5}$/ or die "No match for full prayer times";
+    my %prototype = ( length => $+[0] - $-[0] );
+    $line = substr($line, -$prototype{length});
+    for (@prayer_names) {
+        $line =~ / +(\d\d?) +(\d\d)/g or die "unexpected mismatch at " . pos($line);
+        $prototype{$_} = {
+            hours   => [ $-[1]-1, $+[1]-$-[1]+2 ], # -1: start from 1 char before begin of match
+            minutes => [ $-[2]-1, $+[2]-$-[2]+2 ], # +2: search in 1 more char after end of match
+        };
+    }
+    if (pos($line) != length($line)) {
+        die "unexpected trailing mismatch [" . substr($line, pos($line)) . "]";
+    }
+    print "prototype: " . Dumper(\%prototype) if $debug;
+    return \%prototype;
+}
+
 sub get_diff_min {
-    my ($year, $id, $prev, $curr) = @_;
-    my $prev_sec = timelocal(0, $prev->{$id}{minutes}, $prev->{$id}{hours}, $prev->{day}, $prev->{month}-1, $year-1900);
-    my $curr_sec = timelocal(0, $curr->{$id}{minutes}, $curr->{$id}{hours}, $curr->{day}, $curr->{month}-1, $year-1900);
+    my ($year, $prayer, $prev, $curr) = @_;
+    my $prev_sec = timelocal(0, $prev->{$prayer}{minutes}, $prev->{$prayer}{hours}, $prev->{day}, $prev->{month}-1, $year-1900);
+    my $curr_sec = timelocal(0, $curr->{$prayer}{minutes}, $curr->{$prayer}{hours}, $curr->{day}, $curr->{month}-1, $year-1900);
     $prev_sec += 24*60*60;
     return ($curr_sec - $prev_sec)/60;
 }
 
 sub diff_sanity {
-    my ($year, $id, $prev, $curr) = @_;
+    my ($year, $prayer, $prev, $curr) = @_;
     my $diff_min = &get_diff_min;
     if (abs($diff_min) > 55) {
         my $hdiff = ($diff_min<=>0) * int((abs($diff_min) + 30) / 60); # rounding
-        $curr->{$id}{hours} -= $hdiff;
+        $curr->{$prayer}{hours} -= $hdiff;
         $diff_min = &get_diff_min;
-        printf("[corrected: %02d:%02d]", $curr->{$id}{hours}, $curr->{$id}{minutes});
+        printf("[autocorrected: %02d:%02d]", $curr->{$prayer}{hours}, $curr->{$prayer}{minutes});
     }
     if (abs($diff_min) > 4) {
-        die "\nabnormal minutes diff to yesterday's $id: $diff_min";
+        die "\nabnormal minutes diff to yesterday's $prayer $diff_min";
     }
     return $diff_min;
 }
 
 my %gradients;
 sub gradient_sanity {
-    my ($id, $diff_min) = @_;
+    my ($prayer, $diff_min) = @_;
     my $sign = $diff_min<=>0;
     return if $sign == 0;
-    if (exists $gradients{$id}{sign} && $sign != $gradients{$id}{sign}) {
-        $gradients{$id}{changes}++;
-        print "[$id gradient change #$gradients{$id}{changes}!]" if $gradients{$id}{changes} > 2;
+    if (exists $gradients{$prayer}{sign} && $sign != $gradients{$prayer}{sign}) {
+        $gradients{$prayer}{changes}++;
+        print "[$prayer gradient change #$gradients{$prayer}{changes}!]" if $gradients{$prayer}{changes} > 2;
     }
-    $gradients{$id}{sign} = $sign;
-    die if ($gradients{$id}{changes} // 0) > 5;
+    $gradients{$prayer}{sign} = $sign;
+    die if ($gradients{$prayer}{changes} // 0) > 5;
 }
 
 sub sanity_check {
-    my ($year, $id, $prev, $curr) = @_;
+    my ($year, $prayer, $prev, $curr) = @_;
+    die "\nhours:minutes could not be determined" unless defined $curr->{$prayer}{hours} && defined $curr->{$prayer}{minutes};
+    return unless $prev->{$prayer};
     my $diff_min = &diff_sanity;
-    gradient_sanity($id, $diff_min);
+    gradient_sanity($prayer, $diff_min);
 }
