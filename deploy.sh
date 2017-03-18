3645a4150ff7ab972ddfad3de6872d780821dec6
Irfan Adilovic <irfanadilovic@gmail.com>
2017-03-18 11:07:19 +0100

    Put all build commands to a new build script

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-adb uninstall ba.vaktija.android.irfan &
-apktool b ba.vaktija.android
-echo android | jarsigner -tsa http://timestamp.digicert.com ba.vaktija.android/dist/ba.vaktija.android.apk android
-wait
-adb install ba.vaktija.android/dist/ba.vaktija.android.apk &
-cp -v ba.vaktija.android/dist/ba.vaktija.android.apk /cygdrive/c/Users/root/Google\ Drive/ba.vaktija.android.irfan.apk
-wait
--- pdftotext.sh
+++
@@ -1,1 +0,0 @@
-pdftotext -layout -nopgbrk "$@"
